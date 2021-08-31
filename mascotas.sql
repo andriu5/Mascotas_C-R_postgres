91bfc324fc0dcc5227fa8e44bc35d8e19ccf4671
andresalvearc <andres.alvear5@gmail.com>
2021-08-31 16:17:56 -0400

    Script PostgreSQL para crear tabla pets en base de datos

---
+++ mascotas.sql
@@ -0,0 +1,34 @@
+-- Table: public.pets
+
+DROP TABLE public.pets;
+
+-- Create the function
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE TABLE public.pets
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(45),
+    type VARCHAR(45),
+	created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.pets
+    OWNER to postgres;
+
+-- Create the trigger
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON pets
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
